adbb7923a8a6daf00205134bdabb897a53056046
Saidumar <yunusxodjaevsaidumar@gmail.com>
2026-01-08 15:51:18 +0500

    Первый коммит с заданием bananas

---
+++ main.js
@@ -0,0 +1,14 @@
+//первое задание
+let number = Number(prompt("Введите число"));
+
+for (let i = 1; i <= number; i++) {
+  if (i === 1) {
+    console.log(i + " banana");
+  } else {
+    console.log(i + " bananas");
+  }
+}
+
+
+
+
